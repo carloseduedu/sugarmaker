fb63d445faef38323b0757fdff3445acc7a1390d
carloseduedu <carloseduedu@gmail.com>
2021-01-03 18:01:51 -0300

    Add files via upload

---
+++ run-mainnet-pool.sh
@@ -0,0 +1,1 @@
+./sugarmaker -a YespowerSugar -o stratum+tcp://1pool.sugarchain.org:3333 -u sugar1qghm8ngwc56f5lwudg8jc3ujepupt2nz5v03r2f -t1
---
+++ run-mainnet-solo.sh
@@ -0,0 +1,1 @@
+./sugarmaker -a YespowerSugar -o http://127.0.0.1:34229 -u RPCUSER -p RPCPASSWORD --coinbase-addr=sugar1qghm8ngwc56f5lwudg8jc3ujepupt2nz5v03r2f -t1
